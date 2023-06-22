37b8ae2925c82f168ef773999a51765a1c7f09b7
Kamrul Hasan <kamrul444216@gmail.com>
2023-06-22 15:35:50 +0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='bg-sky-600 py-5 px-5'>
+                <h1 className='text-white font-bold text-2xl'>ANIVALE Image Search</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
